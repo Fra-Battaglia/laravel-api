1e7b0a3d4db2771925680a93163e3f3a8b9295f4
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-13 17:00:44 +0100

    show added

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,14 +5,31 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Technology;
 
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::all();
+        $projects = Project::with(['technologies', 'type'])->get();
         return response()->json([
             'success' => true,
             'results' => $projects,
         ]);
+    }
+
+    public function show($slug) {
+        $detail = Project::with(['technologies', 'type'])->where('slug', $slug)->first();
+
+        if($detail) {
+            return response()->json([
+                "success" => true,
+                "results" => $detail,
+            ]);
+        } else {
+            return response()->json([
+                "success" => false,
+                "results" => 'no',
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/projects', [ProjectController::class, 'index']);
+Route::resource('projects', ProjectController::class);
+
+// Route::get('/projects', [ProjectController::class, 'index']);
+// Route::get('/projects/{slug}', [ProjectController::class, 'show']);
